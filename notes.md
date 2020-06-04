c9a94395f06a4aff0834689b0189962b051f9a4e
Danielle McCarthy <danielle.mccarthy@levvel.io>
2020-06-03 22:04:00 -0400

    cleaning up notes

---
+++ notes.md
@@ -0,0 +1,32 @@
+# General Notes & Todos
+
+## Data sources
+
+https://covid-19.dataflowkit.com/v1
+
+## Todo
+
+[] Create individual field resolvers
+
+[] Create more schema and retrieve more data sources
+
+[] Clean up Regex stuff, make everything more consistent
+
+[] look at how to work with context (in resolvers for queries and fields)
+
+## General Notes
+
+**6/5/2020**
+
+**5/28/2020**
+
+- how to handle if data doesn't have a field? - empty string or null, personal preference
+- make the queries more dynamic with union types
+
+**5/20/2020**
+
+- The root provides a resolver function for each API endpoint
+  refactor for error handling eventually
+- whole application should have consistent style (camel case, etc.)
+- we can change data type in resolvers as well as field names. Mold data how we want to.
+- inside / outside exclamations. A trailing exclamation mark is used to denote a field that use a non-null type i.e. name: String! An exclamation point after a type specifically designates that type as non-nullable
--- server.js
+++ server.js
@@ -3,18 +3,6 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 const axios = require('axios');
 
-//whole application should have consistent style (camel case, etc.)
-// we can change data type in resolvers 
-
-
-//inside / outside exclamations
-// Construct a schema, using GraphQL schema language
-
-//next step is add an argument with country name 
-
-//https://covid-19.dataflowkit.com/v1
-
-
 var schema = buildSchema(`
 type CovidCase {
     country: String
@@ -32,44 +20,53 @@ type CovidCase {
   }
 `);
 
-// The root provides a resolver function for each API endpoint
-//refactor for error handling eventually
-
-//how to handle if data doesn't have a field? 
-
 const fitToSchema = (info) => {
+  const rx = /[^a-z0-9-]/g;
 
   return {
-    country: info['Country_text'],
-    totalCases: Number(info['Total Cases_text'].replace(/[^a-z0-9-]/g, '')),
-    newCases: Number(info['New Cases_text'].replace(/[^a-z0-9-]/g, '')) ,
-    totalDeaths: Number(info['Total Deaths_text'].replace(/[^a-z0-9-]/g, '')),
-    newDeaths: Number(info['New Deaths_text'].replace(/[^a-z0-9-]/g, '')),
-    totalRecovered: Number(info['Total Recovered_text'].replace(/[^a-z0-9-]/g, '')),
-    activeCases: Number(info['Active Cases_text'].replace(/[^a-z0-9-]/g, '')),
-  }
-}
+    country: info['Country_text'] ? info['Country_text'] : null,
+    totalCases: info['Total Cases_text']
+      ? Number(info['Total Cases_text'].replace(rx, ''))
+      : null,
+    newCases: info['New Cases_text']
+      ? Number(info['New Cases_text'].replace(rx, ''))
+      : null,
+    totalDeaths: info['Total Deaths_text']
+      ? Number(info['Total Deaths_text'].replace(rx, ''))
+      : null,
+    newDeaths: info['New Deaths_text']
+      ? Number(info['New Deaths_text'].replace(rx, ''))
+      : null,
+    totalRecovered: info['Total Recovered_text']
+      ? Number(info['Total Recovered_text'].replace(rx, ''))
+      : null,
+    activeCases: info['Active Cases_text']
+      ? Number(info['Active Cases_text'].replace(rx, ''))
+      : null,
+  };
+};
 
 var root = {
   hello: () => {
     return 'Hello world!';
   },
-  covidCases: async (obj, args, context, info) =>  {
+  covidCases: async (obj, args, context, info) => {
+    const response = await axios.get('https://covid-19.dataflowkit.com/v1');
 
-    const response = await axios.get('https://covid-19.dataflowkit.com/v1')
+    const final = response.data.map((data) => fitToSchema(data));
 
-    const final = response.data.map(data => fitToSchema(data))
-
-    return final 
-
+    return final;
   },
 };
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
